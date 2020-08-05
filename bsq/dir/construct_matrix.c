84216da823461464932bc1f65786dac6b56123ba
Ajowan Brian <abrian@ga-f3.msk.21-school.ru>
2020-08-05 22:37:26 +0300

    edited acc to norm

--- bsq/dir/construct_matrix.c
+++ bsq/dir/construct_matrix.c
@@ -5,9 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abrian <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/05 20:26:05 by abrian            #+#    #+#             */
-/*   Updated: 2020/08/05 22:06:33 by aannett          ###   ########.fr       */
-/*   Updated: 2020/08/05 20:58:25 by aannett          ###   ########.fr       */
+/*   Created: 2020/08/05 22:35:15 by abrian            #+#    #+#             */
+/*   Updated: 2020/08/05 22:36:55 by abrian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +18,20 @@ void	add_const(char **file)
 	int		**intarr;
 	int		*index;
 
-	intfile = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
-    intarr = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
-    intfile = fetch_int_arr(file, str_count_buff(file));
-    intarr = fetch_max_sqr(intfile, file);
-    index = fetch_sqr_indx(fetch_max_nbr(intarr, file), intarr, file);
-    file = empty_to_sqr(fetch_max_nbr(intarr, file), index[0], index[1], file);
-    output_tab(file, str_count_buff(file) + 1);
+	intfile = malloc(sizeof(int **) * ft_strlen(file[1]) *
+	str_count_buff(file));
+	intarr = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
+	intfile = fetch_int_arr(file, str_count_buff(file));
+	intarr = fetch_max_sqr(intfile, file);
+	index = fetch_sqr_indx(fetch_max_nbr(intarr, file), intarr, file);
+	file = empty_to_sqr(fetch_max_nbr(intarr, file), index[0], index[1], file);
+	output_tab(file, str_count_buff(file) + 1);
 }
 
 void	construct_matrix(char *argv)
 {
 	char	**file;
+
 	if (!open_f(argv))
 	{
 		ft_putstr("map error\n");
@@ -50,7 +51,7 @@ void	construct_from_input(char **input)
 	int		**intfile;
 	int		**intarr;
 	int		*index;
-	
+
 	if (!err_catcher(input) || !str_compare(input))
 	{
 		ft_putstr("map error");
@@ -59,6 +60,7 @@ void	construct_from_input(char **input)
 	intfile = fetch_int_arr(input, str_count_buff(input));
 	intarr = fetch_max_sqr(intfile, input);
 	index = fetch_sqr_indx(fetch_max_nbr(intarr, input), intarr, input);
-	input = empty_to_sqr(fetch_max_nbr(intarr, input), index[0], index[1], input);
+	input = empty_to_sqr(fetch_max_nbr(intarr, input), index[0], index[1],
+	input);
 	output_tab(input, str_count_buff(input) + 1);
 }
