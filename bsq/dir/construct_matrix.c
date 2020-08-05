85301cf081cfb219753ef076adb257baf3939e81
Akenja Annett <aannett@ga-f5.msk.21-school.ru>
2020-08-05 22:12:20 +0300

    matrix

--- bsq/dir/construct_matrix.c
+++ bsq/dir/construct_matrix.c
@@ -5,20 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abrian <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/05 21:34:08 by abrian            #+#    #+#             */
-/*   Updated: 2020/08/05 21:34:19 by abrian           ###   ########.fr       */
+/*   Created: 2020/08/05 20:26:05 by abrian            #+#    #+#             */
+/*   Updated: 2020/08/05 22:06:33 by aannett          ###   ########.fr       */
+/*   Updated: 2020/08/05 20:58:25 by aannett          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-void	construct_matrix(char *argv)
+void	add_const(char **file)
 {
-	char	**file;
 	int		**intfile;
 	int		**intarr;
 	int		*index;
 
+	intfile = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
+    intarr = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
+    intfile = fetch_int_arr(file, str_count_buff(file));
+    intarr = fetch_max_sqr(intfile, file);
+    index = fetch_sqr_indx(fetch_max_nbr(intarr, file), intarr, file);
+    file = empty_to_sqr(fetch_max_nbr(intarr, file), index[0], index[1], file);
+    output_tab(file, str_count_buff(file) + 1);
+}
+
+void	construct_matrix(char *argv)
+{
+	char	**file;
 	if (!open_f(argv))
 	{
 		ft_putstr("map error\n");
@@ -30,13 +42,7 @@ void	construct_matrix(char *argv)
 		ft_putstr("map error\n");
 		return ;
 	}
-	intfile = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
-	intarr = malloc(sizeof(int **) * ft_strlen(file[1]) * str_count_buff(file));
-	intfile = fetch_int_arr(file, str_count_buff(file));
-	intarr = fetch_max_sqr(intfile, file);
-	index = fetch_sqr_indx(fetch_max_nbr(intarr, file), intarr, file);
-	file = empty_to_sqr(fetch_max_nbr(intarr, file), index[0], index[1], file);
-	output_tab(file, str_count_buff(file) + 1);
+	add_const(file);
 }
 
 void	construct_from_input(char **input)
