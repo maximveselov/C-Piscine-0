33e8f4908a43d71a5bb5188e31707a7f130148a6
Akenja Annett <aannett@ga-f5.msk.21-school.ru>
2020-08-05 22:41:02 +0300

    builder fixed

--- bsq/matrix_builder.c
+++ bsq/matrix_builder.c
@@ -12,36 +12,44 @@ int        min(int a, int b, int c)
     return (i);
 }
 
+void	**add_mat(int **mat, char **tab, int **ret)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < str_count_buff(tab))
+	{
+		j = 1;
+		while (j < ft_strlen(tab[1]))
+		{
+			if (mat[i][j] == 1)
+                ret[i][j] = min(ret[i][j - 1], ret[i - 1][j], ret[i - 1][j - 1]) + 1;
+            else
+                ret[i][j] = 0;
+            j++;
+		}
+		i++;
+	}
+	return (ret);
+}
+
 int        **get_max_square(int **mat, char **tab)
 {
     int        i;
     int        j;
     int        **ret;
-    printf("\n\n %d \n\n", string_count_from_buffer(tab));
-    ret = (int **)malloc(sizeof(int **) * (string_count_from_buffer(tab) + 1) /*rowcount*/);
+
+    ret = (int **)malloc(sizeof(int **) * (str_count_buff(tab) + 1) /*rowcount*/);
     i = -1;
     j = -1;
-    while (++i < string_count_from_buffer(tab) /*func to get rowcount*/)
+    while (++i < str_count_buff(tab) /*func to get rowcount*/)
     {
         ret[i] = malloc(sizeof(int *) * (ft_strlen(tab[1]) + 1));
-        ret[i][0] = mat[i][0];        // ret[i][0] = malloc(sizeof(int)) ??
+        ret[i][0] = mat[i][0];
     }
     while (++j < ft_strlen(tab[1]))
-        ret[0][j] = mat[0][j];        // ret[0][j] = malloc(sizeof(int)) ??
-    i = 1;
-    while (i < string_count_from_buffer(tab) /*rowcount*/)
-    {
-        j = 1;
-        while (j < ft_strlen(tab[1]))
-        {
-            if (mat[i][j] == 1)
-                ret[i][j] = min(ret[i][j - 1], ret[i - 1][j], ret[i - 1][j - 1]) + 1;
-            else
-                ret[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
-//    free(ret);
+        ret[0][j] = mat[0][j];
+	add_mat(mat, tab, ret);	
     return (ret);
 }
