2fbc1929f2afc1f714a064064fae0ed740caca80
Ajowan Brian <abrian@ga-h3.msk.21-school.ru>
2020-08-05 16:38:10 +0300

    rewrite functions' names: drawer.c, ft_convert.c, matrix_builder.c, matrix_reader.c

---
+++ bsq/dir/drawer.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+char	**empty_to_sqr(int size, int string, int pos, char **tmp)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < size)
+	{
+		b = 0;
+		while (b < size)
+		{
+			tmp[string - a][pos - b - 1] = 'x';// допилю потом, как это правильно сделать, сейчас просто на кресты меняю
+			b++;
+		}
+		a++;
+	}
+	return (tmp);
+}
+
+void	map_out_arr(char **answer)
+{
+	while (answer)
+	{;
+		while (*answer)
+		{
+			write (1, *answer, 1);
+			(*answer)++;
+		}
+		answer++;
+	}
+}
