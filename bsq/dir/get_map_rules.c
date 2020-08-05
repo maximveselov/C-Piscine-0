9634cf52a4e24fca392c35d55131726faa248201
Ajowan Brian <abrian@ga-f3.msk.21-school.ru>
2020-08-05 20:07:25 +0300

    old name fixed

--- bsq/dir/get_map_rules.c
+++ bsq/dir/get_map_rules.c
@@ -6,7 +6,7 @@
 /*   By: abrian <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 19:26:56 by abrian            #+#    #+#             */
-/*   Updated: 2020/08/05 19:27:41 by abrian           ###   ########.fr       */
+/*   Updated: 2020/08/05 20:06:48 by abrian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,5 @@ char	fetch_full_char(char **c)
 
 int		fetch_line_nbr(char *optional, char empty, char obstacle, char full)
 {
-	return (read_option_str(optional, &empty, &obstacle, &full));
+	return (read_str_line_nbr(optional, &empty, &obstacle, &full));
 }
